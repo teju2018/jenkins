0b5f9ac2acfc41c1aa77f667144a8997c0f5c279
teja <tejpaulsingadi@gmail.com>
2025-02-11 17:18:31 +0530

    Create main.tf

---
+++ terrform/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  credentials = file("keys.json")
+  project     = "ferrous-upgrade-446608-k0"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+locals {
+  instance_name = "${terraform.workspace}-instance"
+}
+resource "google_compute_instance" "my-instance" {
+  name         = var.name
+  zone         = var.zone
+  machine_type = var.machine_type
+  boot_disk {
+    initialize_params {
+      image = "centos-stream-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+  labels = {
+    name = local.instance_name
+  }
+}
